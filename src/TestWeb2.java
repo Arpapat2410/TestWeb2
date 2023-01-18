f10cf57baf7962effedec45b0a5ed378cbc7f053
Arpapat2410 <120714445+Arpapat2410@users.noreply.github.com>
2023-01-18 15:18:44 +0700

    Initial commit

---
+++ src/TestWeb2.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+class TestWeb2 {
+
+	@Test
+	void test() throws InterruptedException {
+		WebDriver driver = null;
+
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("https://katalon-demo-cura.herokuapp.com/");
+		driver.findElement(By.id("btn-make-appointment")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("txt-username")).sendKeys("John Doe");
+		driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+		Thread.sleep(2000);
+		driver.findElement(By.id("btn-login")).click();
+		Select option1 = new Select(driver.findElement(By.id("combo_facility")));
+		option1.selectByVisibleText("Hongkong CURA Healthcare Center");
+		driver.findElement(By.id("chk_hospotal_readmission")).click();
+		WebElement radio1 = driver.findElement(By.id("radio_program_medicaid"));
+		radio1.click();
+		driver.findElement(By.id("txt_visit_date")).sendKeys("18/01/2023");
+		driver.findElement(By.id("txt_comment")).sendKeys("Heart");
+		Thread.sleep(2000);
+		driver.findElement(By.id("btn-book-appointment")).click();
+	    
+		Thread.sleep(1000);
+		driver.quit();
+        driver = null;
+        
+        
+	}
+
+}
